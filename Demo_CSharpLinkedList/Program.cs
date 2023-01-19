aac579f1ad990f389d1655444db89e3fe1651edb
Terry Nguyen <terry@terrehbyte.com>
2023-01-18 16:57:54 -0800

    Add quick demo of LL in C#

---
+++ Demo_CSharpLinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// C++ std::list => Linked List
+// C# List<T> => Dynamic Array
+
+// C# LinkedList<T> => Linked List
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> numbers = new LinkedList<int>();
+
+        // O(1)
+        var node = numbers.AddLast(5);
+
+        // O(1)
+        numbers.Remove(node);
+
+        // O(N)
+        numbers.Remove(5);
+    }
+}
